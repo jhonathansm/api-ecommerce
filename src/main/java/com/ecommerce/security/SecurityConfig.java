f3d0a5084670a36eee30c550bac751d8f9c5d104
Jhonathan <sjhonatan2012@gmail.com>
2021-08-28 16:55:50 -0300

    Inclusão de configuração cors

--- src/main/java/com/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.security.authentication.AuthenticationManager;
 
 
@@ -42,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+				.and().csrf().disable()
 				.authorizeRequests()
 				.anyRequest().authenticated();
 		
